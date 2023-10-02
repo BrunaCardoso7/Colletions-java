684493a0bb95992ac135a863408374e314d78313
BrunaCardoso7 <obitadrawing@gmail.com>
2023-10-01 23:12:28 -0300

    metodo de verificação implementado

--- jogo-da-senha/src/JogoDaSenha.java
+++ jogo-da-senha/src/JogoDaSenha.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class JogoDaSenha {
-    private int[] chosenRandomValues;
+    private int[] chosenRandomValues; //array com os numeros aleatórios gerados
     private final Random generator;
 
     public JogoDaSenha() {
@@ -12,23 +12,22 @@ public class JogoDaSenha {
     private int[] getChosenRandomValues() {
         chosenRandomValues = new int[30];
         for(int n = 0; n < 30; n++){
-            int randomNumber 
+            int randomNumber;
+            do{
+                randomNumber = generator.nextInt(101);
+            }while(!isUnique(randomNumber, n));
 
+            chosenRandomValues[n] = randomNumber;
         }
         return chosenRandomValues;
     }
-    private boolean mapArray(){
-        boolean result=true;
-        for(int i : chosenRandomValues){
-            for(int n = 0; n<30 ; n++){
-               if(chosenRandomValues[n] != i){
-                   return result = true;
-               }else {
-                   return result = false;
-               }
+    private boolean isUnique(int randomNum, int currentIndex){
+        for(int i = 0; i < currentIndex; i++){
+            if(chosenRandomValues[i] == randomNum){
+                return false;
             }
         }
-        return result;
+        return true;
     }
     public void showValuesGenerator(){
         System.out.println(Arrays.toString(chosenRandomValues));
